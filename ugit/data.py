5866e755bfaa2a9018d01f4c223f267eae40bd21
Son Do <dominhson580@gmail.com>
2026-08-23 22:49:31 +0700

    ugit cat-file

--- ugit/data.py
+++ ugit/data.py
@@ -9,8 +9,13 @@ def init():
     os.makedirs(f"{GIT_DIR}/objects")
 
 
-def hash_object(data: str):
+def hash_object(data: bytes) -> str:
     oid = hashlib.sha1(data).hexdigest()
     with open(f"{GIT_DIR}/objects/{oid}", "wb") as out:
         out.write(data)
     return oid
+
+
+def get_object(oid: str) -> bytes:
+    with open(f"{GIT_DIR}/objects/{oid}", "rb") as f:
+        return f.read()
