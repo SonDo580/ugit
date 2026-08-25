97ddd0db0088fd5da054ba95ce4b41133df59eb1
Son Do <dominhson580@gmail.com>
2026-08-25 17:34:18 +0700

    ugit reset

--- ugit/base.py
+++ ugit/base.py
@@ -115,6 +115,10 @@ def checkout(name: str):
     data.update_ref("HEAD", HEAD, deref=False)
 
 
+def reset(oid: str):
+    data.update_ref("HEAD", data.RefValue(symbolic=False, value=oid))
+
+
 def create_tag(name: str, oid: str):
     data.update_ref(f"refs/tags/{name}", data.RefValue(symbolic=False, value=oid))
 
